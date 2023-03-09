ae0a8d5898bd16a9531c6f7562058de0762f5dca
sokolov123 <anton2003zxcegth@gmail.com>
2023-03-09 12:56:19 +0300

    task4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 function getRandom(min, max) {
   if (min >= max) {
-    console.log('Пожалуйста, введите корректные данные');
-    return -1; //функция вернёт -1 как заведомо невозможное число
+    return 'Пожалуйста, введите корректные данные';
   }
   return Math.random() * (max - min) + min;
 }
-
-console.log(Math.round(getRandom(1, 10000)));
-
 
 function getLenString(lenStr, n) {
   if (lenStr.length >= n) {
@@ -17,6 +13,3 @@ function getLenString(lenStr, n) {
     return true;
   }
 }
-
-
-console.log(getLenString('adfsadfbadrhh', 33));
