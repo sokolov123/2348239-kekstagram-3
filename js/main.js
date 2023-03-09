10bc79ab4eab70fc72a0da6c5cfe4d90dac4a88f
sokolov123 <anton2003zxcegth@gmail.com>
2023-03-09 10:59:26 +0300

    task4

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function getRandom(min, max) {
+  if (min >= max) {
+    return 'Пожалуйста, введите корректные данные';
+  }
+  return  Math.floor(Math.random() * (max - min) + min);
+}
+
+let data = [];
+let i = 1;
+for (i = 1; i < 26; i++) {
+  data.push({id: i, url: `photos/${i}.jpg`, description: photoDescription(), likes: getRandom(15, 200), comments: getRandom(0, 200)});
+}
+
+//пусть будет так, чтобы были рандомные описания фото
+function photoDescription() {
+  const comments = ['ничоси', 'прикольная фотка', 'интересненько', 'я комментарий', 'крутая фотокарточка',
+    'Виталь, ты же говорил ты в командировке', 'так им и надо', 'не ну вы посмтрите на это...', 'делааааа.....'];
+  return comments[getRandom(0, 8)];
+}
