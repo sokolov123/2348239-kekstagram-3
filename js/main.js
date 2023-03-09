1433d8b325367fe68b36797c16f7d1d0f80f3b4c
sokolov123 <anton2003zxcegth@gmail.com>
2023-03-09 11:43:50 +0300

    task4

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function getRandom(min, max) {
   return  Math.floor(Math.random() * (max - min) + min);
 }
 
-let data = [];
+const data = [];
 let i = 1;
 for (i = 1; i < 26; i++) {
   data.push({id: i, url: `photos/${i}.jpg`, description: photoDescription(), likes: getRandom(15, 200), comments: getRandom(0, 200)});
