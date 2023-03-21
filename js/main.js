d8fa29806f5bcf8cc4dd869a5c60c8a6c794b165
sokolov123 <anton2003zxcegth@gmail.com>
2023-03-21 16:42:26 +0300

    module7-task1 try1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 import {getData} from './data.js';
-const users = 25;
-getData(users);
+const usersNum = 25;
+
+const similarListElement = document.querySelector('.pictures');
+const similarPicture = document.querySelector('#picture').content.querySelector('.picture');
+
+const similarPictInfo = similarPicture.querySelector('.picture__info');
+const photo = getData(usersNum);
+
+const similarListFragment = document.createDocumentFragment();
+
+photo.forEach((url, lickes, comments) => {
+  const pictElement = similarPicture.cloneNode(true);
+  const pictInfo = similarPictInfo.cloneNode(true);
+  pictElement.querySelector('.picture__img').src = url;
+  pictInfo.querySelector('.picture__likes').textContent = lickes;
+  pictInfo.querySelector('.picture__comments').textContent = comments;
+  similarListElement.appendChild(pictElement);
+});
+
+similarListElement.appendChild(similarListFragment);
+
