ecf4ee5c7f600adc1f3fd433e4b37ab0350ae912
sokolov123 <anton2003zxcegth@gmail.com>
2023-04-01 16:17:42 +0300

    module7-task2 исправление ошибок

--- js/data.js
+++ js/data.js
@@ -1,9 +1,13 @@
-import {getRandom, photoDescription} from './functions.js';
+import {getRandom, getPhotoDescription} from './functions.js';
+
+const likesMIN = 15;
+const likesMAX = 200;
+const commentNumber = 200;
 
 export function getData(max) { // MAX это количество пользователей
   const data = [];
   for (let i = 1; i <= max; i++) {
-    data.push({id: i, url: `photos/${i}.jpg`, description: photoDescription(), likes: getRandom(15, 200), comments: getRandom(0, 200)});
+    data.push({id: i, url: `photos/${i}.jpg`, description: getPhotoDescription(), likes: getRandom(likesMIN, likesMAX), comments: getRandom(0, commentNumber)});
   }
   return data;
 }
--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
+import {renderPhotos} from './renderPhotos.js';
 import {getData} from './data.js';
+
 const usersNum = 25;
 
-const similarListElement = document.querySelector('.pictures');
-const similarPicture = document.querySelector('#picture').content.querySelector('.picture');
+const photos = getData(usersNum);
 
-const similarPictInfo = similarPicture.querySelector('.picture__info');
-const photo = getData(usersNum);
-
-const similarListFragment = document.createDocumentFragment();
-
-photo.forEach((url, likes, comments) => {
-  const pictElement = similarPicture.cloneNode(true);
-  const pictInfo = similarPictInfo.cloneNode(true);
-  pictElement.querySelector('.picture__img').src = url;
-  pictInfo.querySelector('.picture__likes').textContent = likes;
-  pictInfo.querySelector('.picture__comments').textContent = comments;
-  similarListElement.appendChild(pictElement);
-});
-// Задание 7 часть 2
-similarListElement.appendChild(similarListFragment);
+renderPhotos(photos);
